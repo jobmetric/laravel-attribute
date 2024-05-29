e401965ce462d25cc3a35b6d442efc4aff3cd03c
majid mohammadian <majeedmohammadian@gmail.com>
2024-05-29 23:20:13 +0330

    add service provider

---
+++ src/AttributeServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace JobMetric\Attribute;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class AttributeServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws MigrationFolderNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-attribute')
+            ->hasConfig()
+            ->hasMigration()
+            ->hasTranslation();
+    }
+}
