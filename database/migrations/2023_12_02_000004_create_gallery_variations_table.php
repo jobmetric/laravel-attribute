693fa12043b80fb9734b4b2ba39fc0ce57042797
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-28 00:02:47 +0330

    added created_at field in gallery variation table

--- database/migrations/2023_12_02_000004_create_gallery_variations_table.php
+++ database/migrations/2023_12_02_000004_create_gallery_variations_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use JobMetric\Attribute\Enums\AttributeTypeEnum;
 
@@ -17,6 +18,8 @@ return new class extends Migration {
             $table->id();
 
             $table->morphs('galleryable');
+
+            $table->dateTime('created_at')->nullable()->default(DB::raw('CURRENT_TIMESTAMP'));
         });
     }
 
