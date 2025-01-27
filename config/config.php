7ac3abe220311221f9f0e0dea8d79b0dcdd072bf
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-27 22:35:08 +0330

    added table field in config file

--- config/config.php
+++ config/config.php
@@ -17,4 +17,21 @@ return [
 
     "cache_time" => env("ATTRIBUTE_CACHE_TIME", 0),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Table Name
+    |--------------------------------------------------------------------------
+    |
+    | Table name in database
+    */
+
+    "tables" => [
+        'attribute' => 'attributes',
+        'attribute_value' => 'attribute_values',
+        'attribute_relation' => 'attribute_relations',
+        'attribute_relation_value' => 'attribute_relation_values',
+        'gallery_variation' => 'gallery_variations',
+        'gallery_variation_attribute_value' => 'gallery_variation_attribute_values',
+    ],
+
 ];
