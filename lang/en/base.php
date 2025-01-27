3b93f47203f924a10dd26dc8a76905137b37d77c
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-27 22:35:19 +0330

    added languages files

---
+++ lang/en/base.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Attribute Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Attribute for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "validation" => [
+    ],
+
+    "messages" => [
+    ],
+
+    "exceptions" => [
+    ],
+
+];
