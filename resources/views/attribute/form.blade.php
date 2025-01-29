8c73dcd7ff26e7d955bd3bc3ef16fe4cfa4caf5e
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-30 01:38:23 +0330

    added crud attribute

---
+++ resources/views/attribute/form.blade.php
@@ -0,0 +1,85 @@
+@extends('panelio::layout.layout')
+
+@section('body')
+    <form method="post" action="{{ $action }}" class="form d-flex flex-column flex-lg-row" id="form">
+        @csrf
+        @if($mode === 'edit')
+            @method('put')
+        @endif
+        <div class="d-flex flex-column flex-row-fluid gap-7 gap-lg-12">
+            <div class="tab-content">
+                <div class="tab-pane fade show active">
+                    <div class="d-flex flex-column gap-7 gap-lg-10">
+                        @php
+                            $trans_scope = 'attribute::base.form.attribute.fields.{field}';
+                        @endphp
+                        @if($mode === 'create')
+                            @php
+                                $translation_values = [];
+                                foreach($translations as $item) {
+                                    $translation_locale = app()->getLocale();
+                                    $translation_values[$item] = old("translation.$translation_locale.$item");
+                                }
+                            @endphp
+                            <x-translation-card :items="$translations" :values="$translation_values" :trans-scope="$trans_scope" />
+                        @endif
+
+                        @if($mode === 'edit')
+                            @php
+                                $translation_values = [];
+                                foreach ($languages as $language) {
+                                    foreach($translations as $item) {
+                                        $translation_values[$language->locale][$item] = old("translation.$language->locale.$item", $translation_edit_values[$language->locale][$item] ?? null);
+                                    }
+                                }
+                            @endphp
+                            <x-translation-card :items="$translations" :values="$translation_values" :trans-scope="$trans_scope" multiple />
+                        @endif
+
+                        <!--begin::Information-->
+                        <div class="card card-flush py-4">
+                            <div class="card-header">
+                                <div class="card-title">
+                                    <span class="fs-5 fw-bold">{{ trans('package-core::base.cards.proprietary_info') }}</span>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div class="mb-10">
+                                    <label class="form-label">{{ trans('attribute::base.form.attribute.fields.type.title') }}</label>
+                                    <select name="type" class="form-select" data-control="select2">
+                                        <option value="">{{ trans('package-core::base.select.none') }}</option>
+                                        @foreach($types as $type)
+                                            <option value="{{ $type }}" @if(old('type', $attribute->type ?? null) == $type) selected @endif>{{ trans('attribute::base.enums.attribute_type.' . $type) }}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('type')
+                                        <div class="form-errors text-danger fs-7 mt-2">{{ $message }}</div>
+                                    @enderror
+                                </div>
+                                <div class="mb-10">
+                                    <label class="form-label">{{ trans('attribute::base.form.attribute.fields.ordering.title') }}</label>
+                                    <input type="number" name="ordering" class="form-control mb-2" placeholder="{{ trans('attribute::base.form.attribute.fields.ordering.placeholder') }}" value="{{ old('type', $attribute->ordering ?? null) }}">
+                                    @error('ordering')
+                                        <div class="form-errors text-danger fs-7 mt-2">{{ $message }}</div>
+                                    @enderror
+                                </div>
+
+                                <div class="d-flex align-items-center gap-10">
+                                    <div class="form-check form-check-custom form-check-solid form-check-sm">
+                                        <input type="checkbox" name="is_filter" class="form-check-input" value="" id="input-is-filter"/>
+                                        <label class="form-check-label" for="input-is-filter">{{ trans('attribute::base.form.attribute.fields.is_filter.title') }}</label>
+                                    </div>
+                                    <div class="form-check form-check-custom form-check-solid form-check-sm">
+                                        <input type="checkbox" name="is_special" class="form-check-input" value="" id="input-is-special"/>
+                                        <label class="form-check-label" for="input-is-special">{{ trans('attribute::base.form.attribute.fields.is_special.title') }}</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--end::Information-->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
---
+++ src/Http/Controllers/AttributeController.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace JobMetric\Attribute\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use JobMetric\Attribute\Enums\AttributeTypeEnum;
+use JobMetric\Attribute\Facades\Attribute as AttributeFacade;
+use JobMetric\Attribute\Http\Requests\SetTranslationRequest;
+use JobMetric\Attribute\Http\Requests\StoreAttributeRequest;
+use JobMetric\Attribute\Http\Requests\UpdateAttributeRequest;
+use JobMetric\Attribute\Http\Resources\AttributeResource;
+use JobMetric\Attribute\Models\Attribute;
+use JobMetric\Attribute\Models\Attribute as AttributeModel;
+use JobMetric\Language\Facades\Language;
+use JobMetric\Panelio\Facades\Breadcrumb;
+use JobMetric\Panelio\Facades\Button;
+use JobMetric\Panelio\Facades\Datatable;
+use JobMetric\Panelio\Http\Controllers\Controller;
+use Throwable;
+
+class AttributeController extends Controller
+{
+    private array $route;
+
+    public function __construct()
+    {
+        if (request()->route()) {
+            $parameters = request()->route()->parameters();
+
+            $this->route = [
+                'index' => route('attribute.index', $parameters),
+                'create' => route('attribute.create', $parameters),
+                'store' => route('attribute.store', $parameters),
+                'options' => route('attribute.options', $parameters),
+                'set_translation' => route('attribute.set-translation', $parameters),
+            ];
+        }
+    }
+
+    /**
+     * Display a listing of the attribute.
+     *
+     * @param string $panel
+     * @param string $section
+     *
+     * @return View|JsonResponse
+     * @throws Throwable
+     */
+    public function index(string $panel, string $section): View|JsonResponse
+    {
+        if (request()->ajax()) {
+            $query = AttributeFacade::query(with: ['translations', 'attributeRelations']);
+
+            return Datatable::of($query, resource_class: AttributeResource::class);
+        }
+
+        $data['label'] = trans('attribute::base.list.attribute.title');
+        $data['description'] = trans('attribute::base.list.attribute.description');
+
+        DomiTitle($data['label']);
+
+        // Add breadcrumb
+        add_breadcrumb_base($panel, $section);
+        Breadcrumb::add($data['label']);
+
+        // add button
+        Button::add($this->route['create']);
+        Button::delete();
+
+        DomiLocalize('attribute', [
+            'route' => $this->route['index'],
+        ]);
+
+        DomiPlugins('jquery.form');
+
+        $translation = (new Attribute)->translationAllowFields();
+
+        DomiAddModal('translation', '', view('translation::modals.translation-array-list', [
+            'action' => $this->route['set_translation'],
+            'items' => $translation,
+            'trans_scope' => 'attribute::base.form.attribute.fields.{field}',
+        ]), options: [
+            'size' => 'lg'
+        ]);
+
+        DomiScript('assets/vendor/attribute/js/attribute/list.js');
+
+        $data['route'] = $this->route['options'];
+
+        return view('attribute::attribute.list', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param string $panel
+     * @param string $section
+     *
+     * @return View
+     */
+    public function create(string $panel, string $section): View
+    {
+        $data['mode'] = 'create';
+
+        $list_label = trans('attribute::base.list.attribute.title');
+        $data['label'] = trans('attribute::base.form.attribute.create.title');
+        $data['description'] = trans('attribute::base.form.attribute.create.description');
+
+        DomiTitle($data['label']);
+
+        // Add breadcrumb
+        add_breadcrumb_base($panel, $section);
+        Breadcrumb::add($list_label, $this->route['index']);
+        Breadcrumb::add($data['label']);
+
+        // add button
+        Button::save();
+        Button::saveNew();
+        Button::saveClose();
+        Button::cancel($this->route['index']);
+
+        DomiScript('assets/vendor/attribute/js/attribute/form.js');
+
+        $data['action'] = $this->route['store'];
+        $data['translations'] = (new Attribute)->translationAllowFields();
+        $data['types'] = AttributeTypeEnum::values();
+
+        return view('attribute::attribute.form', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreAttributeRequest $request
+     * @param string $panel
+     * @param string $section
+     *
+     * @return RedirectResponse
+     * @throws Throwable
+     */
+    public function store(StoreAttributeRequest $request, string $panel, string $section): RedirectResponse
+    {
+        $form_data = $request->all();
+
+        $attribute = AttributeFacade::store($request->validated());
+
+        if ($attribute['ok']) {
+            $this->alert($attribute['message']);
+
+            if ($form_data['save'] == 'save.new') {
+                return back();
+            }
+
+            if ($form_data['save'] == 'save.close') {
+                return redirect()->to($this->route['index']);
+            }
+
+            // btn save
+            return redirect()->route('attribute.edit', [
+                'panel' => $panel,
+                'section' => $section,
+                'jm_attribute' => $attribute['data']->id
+            ]);
+        }
+
+        $this->alert($attribute['message'], 'danger');
+
+        return back();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param string $panel
+     * @param string $section
+     * @param AttributeModel $attribute
+     *
+     * @return View
+     */
+    public function edit(string $panel, string $section, AttributeModel $attribute): View
+    {
+        $attribute->load(['translations']);
+
+        $data['mode'] = 'edit';
+
+        $list_label = trans('attribute::base.list.attribute.title');
+        $data['label'] = trans('attribute::base.form.attribute.edit.title', [
+            'number' => $attribute->id
+        ]);
+        $data['description'] = trans('attribute::base.form.attribute.edit.description', [
+            'number' => $attribute->id
+        ]);
+
+        DomiTitle($data['label']);
+
+        // Add breadcrumb
+        add_breadcrumb_base($panel, $section);
+        Breadcrumb::add($list_label, $this->route['index']);
+        Breadcrumb::add($data['label']);
+
+        // add button
+        Button::save();
+        Button::saveNew();
+        Button::saveClose();
+        Button::cancel($this->route['index']);
+
+        DomiScript('assets/vendor/attribute/js/attribute/form.js');
+
+        $data['action'] = route('attribute.update', [
+            'panel' => $panel,
+            'section' => $section,
+            'jm_attribute' => $attribute->id
+        ]);
+
+        $data['attribute'] = $attribute;
+        $data['languages'] = Language::all();
+        $data['translations'] = (new Attribute)->translationAllowFields();
+        $data['types'] = AttributeTypeEnum::values();
+        $data['translation_edit_values'] = translationResourceData($attribute->translations);
+
+        return view('attribute::attribute.form', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param UpdateAttributeRequest $request
+     * @param string $panel
+     * @param string $section
+     * @param AttributeModel $attribute
+     *
+     * @return RedirectResponse
+     * @throws Throwable
+     */
+    public function update(UpdateAttributeRequest $request, string $panel, string $section, AttributeModel $attribute): RedirectResponse
+    {
+        $form_data = $request->all();
+
+        $attribute = AttributeFacade::update($attribute->id, $request->validated());
+
+        if ($attribute['ok']) {
+            $this->alert($attribute['message']);
+
+            if ($form_data['save'] == 'save.new') {
+                return redirect()->to($this->route['create']);
+            }
+
+            if ($form_data['save'] == 'save.close') {
+                return redirect()->to($this->route['index']);
+            }
+
+            // btn save
+            return redirect()->route('attribute.edit', [
+                'panel' => $panel,
+                'section' => $section,
+                'jm_attribute' => $attribute['data']->id
+            ]);
+        }
+
+        $this->alert($attribute['message'], 'danger');
+
+        return back();
+    }
+
+    /**
+     * Delete the specified resource from storage.
+     *
+     * @param array $ids
+     * @param mixed $params
+     * @param string|null $alert
+     * @param string|null $danger
+     *
+     * @return bool
+     * @throws Throwable
+     */
+    public function deletes(array $ids, mixed $params, string &$alert = null, string &$danger = null): bool
+    {
+        try {
+            foreach ($ids as $id) {
+                AttributeFacade::delete($id);
+            }
+
+            $alert = trans_choice('attribute::base.messages.attribute.deleted_items', count($ids));
+
+            return true;
+        } catch (Throwable $e) {
+            $danger = $e->getMessage();
+
+            return false;
+        }
+    }
+
+    /**
+     * Set Translation in list
+     *
+     * @param SetTranslationRequest $request
+     *
+     * @return JsonResponse
+     * @throws Throwable
+     */
+    public function setTranslation(SetTranslationRequest $request): JsonResponse
+    {
+        try {
+            return $this->response(
+                AttributeFacade::setTranslation($request->validated())
+            );
+        } catch (Throwable $exception) {
+            return $this->response(message: $exception->getMessage(), status: $exception->getCode());
+        }
+    }
+}
