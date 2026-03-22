e44c8852363592fafcffec6a818dc78f0a6a686f
majidmohammadian <majeedmohammadian@gmail.com>
2026-03-22 18:46:27 +0330

    changed config file for removed unused config key

--- config/config.php
+++ config/config.php
@@ -4,28 +4,13 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Cache Time
-    |--------------------------------------------------------------------------
-    |
-    | Cache time for get data attribute
-    |
-    | - set zero for remove cache
-    | - set null for forever
-    |
-    | - unit: minutes
-    */
-
-    "cache_time" => env("ATTRIBUTE_CACHE_TIME", 0),
-
-    /*
-    |--------------------------------------------------------------------------
     | Table Name
     |--------------------------------------------------------------------------
     |
     | Table name in database
     */
 
-    "tables" => [
+    'tables' => [
         'attribute' => 'attributes',
         'attribute_value' => 'attribute_values',
         'attribute_relation' => 'attribute_relations',
